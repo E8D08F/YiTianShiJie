cc0105a6b03cf718047caa424d11565dc83ad260
Toto Minai <the@unpopular.me>
2022-09-28 14:02:50 +0900

    Implement backup

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,34 +1,63 @@
 import Parser from 'rss-parser'
 
+const baseURL = process.env.BASE_URL
+
 let parser = new Parser({
     customFields: {
         item: [
-            ['content:encoded', 'fullContent']
+            [ 'content:encoded', 'fullContent' ]
         ]
     }
 })
 
-export const getAllPostIds = async () => {
-    const feed = await parser.parseURL('https://blog.yitianshijie.net/feed')
+interface Post {
+    title: string
+    author: string
+    id: string
+    slug: string
+    description: string
+    content: string
+}
 
-    return feed.items.flatMap(item => {
+export const getAllPostIds = async () => {
+    let remoteIDs = new Set<string>()
+    const feed = await parser.parseURL('https://blog.yitianshijie.net/feed')
+    const remotePosts = feed.items.flatMap(item => {
+        if (!item.guid) { return }
+        remoteIDs.add(item.guid)
+
         const re = /blog\.yitianshijie\.net\/(20[0-9]{2})\/([01]\d)\/([0-3]\d)\/([^\/]+)\/?$/
         if (item.link != undefined) {
             const matched = item.link.match(re)
             if (matched != undefined) {
-
-                if (item.guid != undefined) {
-                    return [{
-                        params: {
-                            id: [ matched[1], matched[2], matched[3], matched[4] ]
-                        }
-                    }]
-                }
+                return [{
+                    params: {
+                        id: [ matched[1], matched[2], matched[3], matched[4] ]
+                    }
+                }]
             }
         }
 
         return []
     })
+
+    const response = await fetch(`${baseURL}/backup.json`)
+    const backupPosts = await response.json() as Post[]
+    const localPosts = backupPosts.filter(post => !remoteIDs.has(post.id)).map(post => {
+        const re = /(20[0-9]{2})\/([01]\d)\/([0-3]\d)\/([^\/]+)\/?$/
+        const matched = post.slug.match(re)
+        if (matched != undefined) {
+            return {
+                params: {
+                    id: [ matched[1], matched[2], matched[3], matched[4] ]
+                }
+            }
+        }
+    })
+
+    const combinedPosts = remotePosts.concat(localPosts)
+
+    return combinedPosts
 }
 
 export const getPostData = async (id: string) => {
@@ -45,7 +74,21 @@ export const getPostData = async (id: string) => {
             author: item.creator,
             link: item.link,
             content: item.fullContent,
-            description: (item.content as string).replace('[&#8230;]', '❧')
+            description: (item.content as string).replace('[&#8230;]', '…')
+        }
+    } else {
+        const response = await fetch(`${baseURL}/backup.json`)
+        const backupPosts = await response.json() as Post[]
+        const found =backupPosts.find(post => post.slug.includes(id))
+
+        if (found != undefined) {
+            return {
+                title: found.title,
+                author: found.author,
+                link: 'https://blog.yitianshijie.net/' + found.slug,
+                content: found.content,
+                description: found.description.replace('[&#8230;]', '…')
+            }
         }
     }
 

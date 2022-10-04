d2e0c18161269fb6e34e19bca3ae8bd0dca93eb9
Toto Minai <the@unpopular.me>
2022-10-04 03:03:42 -0700

    Adjust gradients

--- pages/[...id].tsx
+++ pages/[...id].tsx
@@ -98,11 +98,13 @@ export default function Post({
                   media="(prefers-color-scheme: dark)" />
         </Head>
         <Script type="text/javascript" src="//typesquare.com/3/tsst/script/zh_tw/typesquare.js?631f3e24d50445ffb32d203eac1e02e5&fadein=-1" charSet="utf-8"></Script>
-        <article dangerouslySetInnerHTML={{ __html: postData.content }}>
-        </article>
+        <div className="tategaki-container-1">
+		<div className="tategaki-container-2 overflow-x-auto">
+			<article dangerouslySetInnerHTML={{ __html: postData.content }}>
+			</article>
+		</div>
+	</div>
         
-        <div className="gradient-left"></div>
-        <div className="gradient-right"></div>
     </>
 }
 

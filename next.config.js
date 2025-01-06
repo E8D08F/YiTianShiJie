63c94b7fba0b8fdc8e2279dfd1afab6e4075f382
Toto <8411709+E8D08F@users.noreply.github.com>
2025-01-06 12:17:23 +0900

    Tempt to use node 22.x as default

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // MARK: `swcMinify` isn't considered a valid option after Next.js upgrade
+  // swcMinify: true,
 }
 
 module.exports = nextConfig

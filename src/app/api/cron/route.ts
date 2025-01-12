fef915aa3cadb0d1153abbd7c7a5b8b48c91f7aa
Toto <8411709+E8D08F@users.noreply.github.com>
2025-01-12 20:55:02 +0900

    Cron job to redeploy if newer publication exists

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,69 @@
+import { NextResponse } from "next/server"
+import { Vercel } from "@vercel/sdk"
+
+
+const vercel = new Vercel({
+  bearerToken: process.env["BEARER_TOKEN"],
+})
+
+const PUBLICATION_DATE = /<news:publication_date>([^<]+)<\/news:publication_date>/
+export const GET = async (req: Request) => {
+  try {
+    if (req.headers.get("Authorization") !== `Bearer ${process.env["BEARER_TOKEN"]}`)
+      return NextResponse.json({
+        error: "Unauthorised",
+      }, { status: 401 })
+
+    const publication = await fetch("https://blog.yitianshijie.net/news-sitemap.xml")
+      .then(fetched => fetched.text())
+      .then(raw => {
+        return raw.match(PUBLICATION_DATE)
+      })
+
+    if (!publication)
+      throw new Error("No Publication Information")
+
+    const publication_date = new Date(publication[1])
+
+    const result = await vercel.deployments.getDeployments({
+      app: "yitianshijie",
+      target: "production",
+      state: "READY",
+      limit: 1,
+    })
+
+    if (result.deployments.length == 0)
+      throw new Error("No Deployment Found")
+
+    const deployment = result.deployments[0]
+    const ready = deployment.ready
+    if (!ready)
+      throw new Error("Unready Deployment")
+
+    const redeploy = publication_date.getTime() >= ready
+    if (redeploy) {
+      const { id, status } = await vercel.deployments.createDeployment({
+        requestBody: {
+          name: "yitianshijie",
+          target: "production",
+          deploymentId: deployment.uid,
+        }
+      })
+
+      console.log(`Deployment created: ${id}, status ${status}`)
+    } else { console.log(`No Newer Publication`) }
+
+    return NextResponse.json({
+      ok: true,
+      redeploy,
+      publication: publication_date.getTime(),
+      deployment: ready,
+    })
+  } catch (error) {
+    const message = error instanceof Error ? `Error: ${error.message}` : String(error)
+    console.error(message)
+    return NextResponse.json({
+      error: message,
+    }, { status: 500 })
+  }
+}

4d7d775f9bc1ba4a5309e4ab7c753519333bf437
Toto <8411709+E8D08F@users.noreply.github.com>
2025-01-24 16:19:17 +0900

    Reindent

--- src/app/posts.ts
+++ src/app/posts.ts
@@ -6,13 +6,13 @@ import { convert } from 'html-to-text'
 import Sitemapper from 'sitemapper'
 
 export interface PostData {
-    title: string
-    author: string
-    id?: string
-    slug?: string
-    content: string
-    description?: string
-    link?: string
+  title: string
+  author: string
+  id?: string
+  slug?: string
+  content: string
+  description?: string
+  link?: string
 }
 
 export type Params = {
@@ -23,9 +23,9 @@ export type Params = {
 }
 
 let rssParser = new Parser({
-    customFields: {
-        item: [ [ 'content:encoded', 'fullContent' ] ]
-    }
+  customFields: {
+    item: [ [ 'content:encoded', 'fullContent' ] ]
+  }
 })
 
 const sitemap = new Sitemapper({
@@ -39,12 +39,12 @@ export const getAllPostIds = async (): Promise<Params[]> => {
       const matches = link.match(POST_LINK)
       if (!matches) return []
 
-      return [{
-        year:  matches[1],
-        month: matches[2],
-        day:   matches[3],
-        slug:  matches[4],
-      }]
+        return [{
+          year:  matches[1],
+          month: matches[2],
+          day:   matches[3],
+          slug:  matches[4],
+        }]
     })
   } catch (error) { console.error(error) }
 
@@ -52,112 +52,112 @@ export const getAllPostIds = async (): Promise<Params[]> => {
 }
 
 const getStandardID = (id: string, seperated: boolean=false) => {
-    const re = /(20[0-9]{2})\/([01]\d)\/([0-3]\d)\/([^\/]+)$/
-    const matched = id.match(re)
-    return matched ? (seperated ? matched : matched[0]) : null
+  const re = /(20[0-9]{2})\/([01]\d)\/([0-3]\d)\/([^\/]+)$/
+  const matched = id.match(re)
+  return matched ? (seperated ? matched : matched[0]) : null
 }
 
 const getChunghwanDate = (year: string, month: number, day: number) => {
-    const chunghwanNumbers = [
-        [ '〇', '一', '二', '三', '四', '五', '六', '七', '八', '九' ],
-        [ '', '十', '廿', '卅' ]
-    ]
+  const chunghwanNumbers = [
+    [ '〇', '一', '二', '三', '四', '五', '六', '七', '八', '九' ],
+    [ '', '十', '廿', '卅' ]
+  ]
 
-    const twoDigitsNumber = (x: number) =>
-        chunghwanNumbers[1][Math.floor(x / 10)] +
-        (x % 10 === 0 ? '' : chunghwanNumbers[0][x % 10])
+  const twoDigitsNumber = (x: number) =>
+  chunghwanNumbers[1][Math.floor(x / 10)] +
+    (x % 10 === 0 ? '' : chunghwanNumbers[0][x % 10])
 
-    let date = Array.from(year).map(ch => chunghwanNumbers[0][parseInt(ch)]).join('') + '年'
-    date += twoDigitsNumber(month) + '月'
-    date += twoDigitsNumber(day) + '日'
-    return date
+  let date = Array.from(year).map(ch => chunghwanNumbers[0][parseInt(ch)]).join('') + '年'
+  date += twoDigitsNumber(month) + '月'
+  date += twoDigitsNumber(day) + '日'
+  return date
 }
 
 const getProcessedHTML = ({ link, title, author, content }: PostData) => {
-    let chunghwanDate: string | null = null
-    if (title === '') {
-        const ids = getStandardID(link!, true)!
-        chunghwanDate = getChunghwanDate(
-            ids[1],
-            parseInt(ids[2]),
-            parseInt(ids[3])
-        )
-    }
+  let chunghwanDate: string | null = null
+  if (title === '') {
+    const ids = getStandardID(link!, true)!
+    chunghwanDate = getChunghwanDate(
+      ids[1],
+      parseInt(ids[2]),
+      parseInt(ids[3])
+    )
+  }
 
-    const { window: { document } } = parseHTML(`
-    <!DOCTYPE html>
-    <html>
-    <body>
-        <article>
-            <div id="heading">
-                <h1><a href="${link}">${chunghwanDate ? chunghwanDate : title}</a></h1>
-                <p class="no-indent">${author}</p>
-            </div>
+  const { window: { document } } = parseHTML(`
+  <!DOCTYPE html>
+  <html>
+  <body>
+    <article>
+      <div id="heading">
+        <h1><a href="${link}">${chunghwanDate ? chunghwanDate : title}</a></h1>
+        <p class="no-indent">${author}</p>
+      </div>
 
-            ${content}
-        </article>
-    </body>
-    </html>`)
+      ${content}
+    </article>
+  </body>
+  </html>`)
 
-    let paragraphs = Array.from(document.body.querySelectorAll('p'))
+  let paragraphs = Array.from(document.body.querySelectorAll('p'))
 
-    // Consider `<br>` as a separator of paragraphs
-    // Turn those paragraphs into multiple `<p>`s
-    let quotations = Array.from(document.body.getElementsByTagName('blockquote'))
-    quotations.forEach(quotation => {
-        Array.from(quotation.getElementsByTagName('p')).forEach(p => {
-            p.outerHTML = p.outerHTML.replace(/<br>\s*/g, '</p><p>')
-        })
+  // Consider `<br>` as a separator of paragraphs
+  // Turn those paragraphs into multiple `<p>`s
+  let quotations = Array.from(document.body.getElementsByTagName('blockquote'))
+  quotations.forEach(quotation => {
+    Array.from(quotation.getElementsByTagName('p')).forEach(p => {
+      p.outerHTML = p.outerHTML.replace(/<br>\s*/g, '</p><p>')
     })
+  })
 
-    const article = document.body.querySelector('article')!
-    let para = paragraphs[paragraphs.length-1]
-    const matches = para.innerHTML.trim().match(/(竖)排版）$|([豎竪])排版）$/)
-    const useSimplifiedChinese = matches && matches[1]  // Test if matched the first group (simplified Chinese)
-    if (!matches) {  // Create a new paragraph if no reference found
-      para = document.createElement("p")
-      article.appendChild(para)
-    }
-    para.classList.add('original-post')
-    para.innerHTML = `（<a href=${link}>原${useSimplifiedChinese ? "载" : "載"}</a>《一天世界》博客）`
+  const article = document.body.querySelector('article')!
+  let para = paragraphs[paragraphs.length-1]
+  const matches = para.innerHTML.trim().match(/(竖)排版）$|([豎竪])排版）$/)
+  const useSimplifiedChinese = matches && matches[1]  // Test if matched the first group (simplified Chinese)
+  if (!matches) {  // Create a new paragraph if no reference found
+    para = document.createElement("p")
+    article.appendChild(para)
+  }
+  para.classList.add('original-post')
+  para.innerHTML = `（<a href=${link}>原${useSimplifiedChinese ? "载" : "載"}</a>《一天世界》博客）`
 
-    let tategaki = new Tategaki(article, {
-        imitatePcS: true,
-        shouldAdjustOrphanLine: true
-        // shouldRemoveStyle: true
-        // Browser detection is of no use in generating HTML
-    }, document)
-    tategaki.parse()
+  let tategaki = new Tategaki(article, {
+    imitatePcS: true,
+    shouldAdjustOrphanLine: true
+    // shouldRemoveStyle: true
+    // Browser detection is of no use in generating HTML
+  }, document)
+  tategaki.parse()
 
-    return document.body.innerHTML
+  return document.body.innerHTML
 }
 
 const retrieveDataDirectlyFromWebsite = async (id: string) => {
-    const link = `https://blog.yitianshijie.net/${id}`
+  const link = `https://blog.yitianshijie.net/${id}`
     const r_2 = await fetch(link)
-    const rawHTML = r_2.ok ? await r_2.text() : null
+  const rawHTML = r_2.ok ? await r_2.text() : null
 
-    if (!rawHTML) { return null }
+  if (!rawHTML) { return null }
 
-    const { window: { document } } = parseHTML(rawHTML)
-    const article = document.querySelector('article')
-    if (!article) { return null }
-    const title = convert(article.querySelector('h1.entry-title')!.innerHTML)
-    const author = article.querySelector('.byline .author a')!.innerHTML
-    const contentElement = article.querySelector('.entry-content')! as HTMLElement
-    const content = contentElement.innerHTML
+  const { window: { document } } = parseHTML(rawHTML)
+  const article = document.querySelector('article')
+  if (!article) { return null }
+  const title = convert(article.querySelector('h1.entry-title')!.innerHTML)
+  const author = article.querySelector('.byline .author a')!.innerHTML
+  const contentElement = article.querySelector('.entry-content')! as HTMLElement
+  const content = contentElement.innerHTML
 
 
-    const descriptionRaw = contentElement.innerText.trim().replaceAll('\n', ' ')
-    const description = descriptionRaw.slice(0, Math.min(55, descriptionRaw.length)) + '…'
+  const descriptionRaw = contentElement.innerText.trim().replaceAll('\n', ' ')
+  const description = descriptionRaw.slice(0, Math.min(55, descriptionRaw.length)) + '…'
 
-    return {
-        title,
-        author,
-        content: getProcessedHTML({ link, title, author, content }),
-        description,
-        link,
-    }
+  return {
+    title,
+    author,
+    content: getProcessedHTML({ link, title, author, content }),
+    description,
+    link,
+  }
 }
 
 export const getPostData = async ({ year, month, day, slug }: Params) => {
